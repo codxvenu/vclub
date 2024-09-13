f608eb514639cc40d3e0b13ed05b044c7de3e8ac
venu <vasulallu09@gmail.com>
2024-09-13 13:52:46 +0530

    cc update

--- app/credit-cards/page.js
+++ app/credit-cards/page.js
@@ -142,7 +142,7 @@ const CreditCardForm = () => {
 
     const handleSubmit = async () => {
         try {
-          const response = await fetch('http://localhost:5000/api/cards', {
+          const response = await fetch('/api/cards', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',

e8036f460ff1900923b07e11fdbd9daccfdf9d05
codxvenu <158077441+codxvenu@users.noreply.github.com>
2024-09-28 17:12:25 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'viparraich@gmail.com',
-    pass: 'bria njlv ybqu jtnl' // Your app password
+    pass: 'ftwz rrsb fkhc bgho' // Your app password
   }
 });
 
